4bc5f45c86467a02a857f457e2305e1326eef845
bariotti <45301634+bariotti@users.noreply.github.com>
2020-08-18 21:19:50 -0300

    Remover annotations de validação no DTO

--- WebAPI/DTO/PessoaDTO.cs
+++ WebAPI/DTO/PessoaDTO.cs
@@ -9,13 +9,8 @@ namespace WebApi.DTO
 {
     public class PessoaDTO
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Campo nome é obrigatório!")]
-        [MaxLength(100, ErrorMessage = "Tamanho limite é 100 caracteres")]
         public string Nome { get; set; }
-        [Required]
         public string Cpf { get; set; }
-        [MinLength(0)]
-        [MaxLength(99)]
         public int Idade { get; set; }
         public bool Ativo { get; set; }
         public IEnumerable<Contato> Contatos { get; set; }
